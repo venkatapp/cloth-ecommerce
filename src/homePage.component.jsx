de6f7168f25b0f5f63660d46b91f6921659d557b
venkatappa <venkateshni24@gmil.com>
2020-12-15 06:13:22 +0530

    add first initial files

---
+++ src/homePage.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./homePage.styles.scss";
+
+const homePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="titel">Hats</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="titel">Hats</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="titel">Hats</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="titel">Hats</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="titel">Hats</h1>
+            <span className="sub-title">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default homePage;
